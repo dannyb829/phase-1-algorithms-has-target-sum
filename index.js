fa5ee75276c4fbce09cc9173f453043b23d3c998
Daniel <dannyb829@gmail.com>
2022-03-11 11:57:03 -0500

    commit tree f17f9c674a38cb1a36e3e788cd723d2bedce5bc4

--- index.js
+++ index.js
@@ -1,17 +1,49 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+    for (let i = 0; i <= array.length -1; i++){
+        debugger;
+        for (let i2 = 0; i2 <= array.length - 1; i2++){
+            debugger;
+            if(i !== i2){
+                if (array[i] + array[i2] === target){
+                    return true
+                }
+            }    
+            else if (i === i2){
+                    i2++
+                    if (array[i] + array[i2] === target){
+                        return true
+                    }
+            }
+        
+        }
+    }
+    return false
 }
+
 
 /* 
   Write the Big O time complexity of your function here
+  the time complexity of this function is O(2n) => O(n)
 */
 
 /* 
   Add your pseudocode here
+  iterate over indeces of array
+     for each index
+         iterate over the array and 
+           if first index is not the same as second index add first index value to second index value
+            if the sum equals target
+                return true
+  else return false
+
 */
 
 /*
   Add written explanation of your solution here
+  using two iterations of the same array i am able to check all possible sums for elments one element at a time
+  as long as i am not adding values of the same index. returning true if a match is found.
+  the default return value of this function is false if no match is found
 */
 
 // You can run `node index.js` to view these console logs
